f26a21bae95fc820681ff07f956a680ff1dc6df6
Yessly Mayorga <106281825+YessPuentes12@users.noreply.github.com>
2023-03-09 21:52:37 -0600

    Add files via upload

---
+++ EVA2_5_TIPOS_HERENCIA/src/eva2_5_tipos_herencia/EVA2_5_TIPOS_HERENCIA.java
@@ -0,0 +1,36 @@
+/*
+
+ */
+package eva2_5_tipos_herencia;
+
+
+public class EVA2_5_TIPOS_HERENCIA {
+
+
+    public static void main(String[] args) {
+     
+   // Existen 2 tipos de herencia: múltiple y simole     
+   // Java solo permite simple     
+   // Problemas : coliciones de elementos
+   // Se puede simular herencia con interfaces      
+    }
+   
+    class SuperClase{ 
+        
+    }
+ //------------------------------------------------------------------------------
+    class OtraSuperClase{
+   //No se hereda de +2 superclases   
+        
+    }
+ //------------------------------------------------------------------------------
+    class BaseClase extends SuperClase{
+    //Funciona unicamente para una clase
+        
+    }
+    
+    
+    
+    
+    
+}
